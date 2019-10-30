e0512c96356b7ab4f586c6a50a422864ccb471a6
Mehedi Hasan Nahid <nahid@deligram.com>
2019-10-30 10:54:17 +0600

    fixed: getFromNested() method when some node is null

--- src/Queriable.php
+++ src/Queriable.php
@@ -412,6 +412,8 @@ trait Queriable
             $path = explode($this->_traveler, $node);
 
             foreach ($path as $val) {
+                if (!is_array($val)) return $val;
+
                 if (!array_key_exists($val, $map)) {
                     $terminate = true;
                     break;
